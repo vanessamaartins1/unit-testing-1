f17794e74b31571335b0014049583f075ae28660
Milton Castro <milton.castro@inovant.com.br>
2020-02-27 19:27:04 -0300

    Add my first test

---
+++ src/test/java/school/cesar/unit/MyFirstTest.java
@@ -0,0 +1,13 @@
+package school.cesar.unit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void myFirstTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
